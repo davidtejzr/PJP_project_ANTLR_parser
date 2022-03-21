8dc3ce4d60b2eaa60d21ab9e1c31e4330f8e8a8f
David Tejzr <david.tejzr@seznam.cz>
2022-03-21 16:53:09 +0100

    Add project files.

---
+++ PJP_project_ANTLR_parser/Program.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace PJP_project_ANTLR_parser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = new StreamReader("input1.txt");
+            AntlrInputStream stream = new AntlrInputStream(inputFile);
+            ITokenSource lexer = new MyGrammarLexer(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            MyGrammarParser parser = new MyGrammarParser(tokens);
+            parser.BuildParseTree = true;
+            IParseTree tree = parser.prog();
+            Console.WriteLine(tree.ToStringTree(parser));
+        }
+    }
+}
